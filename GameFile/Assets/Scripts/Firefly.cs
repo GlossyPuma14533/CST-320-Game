ff6856a00dc591407832e35db938eb877582ed15
GlossyPuma14533 <christofferhansen10@icloud.com>
2026-02-14 22:21:42 -0700

    Adding Firefly Puzzle to SnowyMountain

---
+++ GameFile/Assets/Scripts/Firefly.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0e01b78e9896c662a2df6acba7cb44ccff610216e9e3194b57f76fad7f908e1
+size 3103
---
+++ GameFile/Assets/Scripts/FireflyPuzzleManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:853c3ef6602042ea8467645cc0fedbe8dea3fed718e246cca61dbd702e8b35c4
+size 3614
