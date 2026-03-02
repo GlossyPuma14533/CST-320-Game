b95a3346f8671bbe5e5788f8d757ee49fcab77ea
GlossyPuma14533 <christofferhansen10@icloud.com>
2026-03-01 23:28:06 -0700

    Adding Ui with Scripts
    Added UI (Menu, Inventory, Dialogue Box) still needs to be tested in VR to make sure it all works once it does will push to main

---
+++ GameFile/Assets/Scripts/InventoryButton.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ac5e34b70a1a42b3092f6cf7a0f39bb061de8baeea7fa04793b6cb903bbbdaf
+size 464
---
+++ GameFile/Assets/Scripts/ResumeButton.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8bba3e4d69302189edb5b108e716a65acf53e4949c1e7031e0b08137e8a6955
+size 455
